873a1cba1f623a6b5b18c72827ed4338b8b6d45d
olya-ivanova <63055929+olya-ivanova@users.noreply.github.com>
2023-04-24 10:23:06 +0300

    Update find-target-substring-index.js

--- find-target-substring-index.js
+++ find-target-substring-index.js
@@ -1,7 +1,25 @@
+// 1
+
 const str = "Hello, Hello, World!";
 const target = "Hello";
 
 let position = -1;
 while ((position = str.indexOf(target, ++position)) !== -1) {
     alert(position);
+}
+
+// 2
+
+const str = "Hello, Hello";
+const target = "Hello";
+let position = 0;
+
+while(true) {
+    let index = str.indexOf(target, position);
+    if (index === -1) {
+        break;
+    }
+    
+    position = index + 1;
+    alert(index);
 }
